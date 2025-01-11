0f020e8354adf344e23679dc07f171d5f6314acc
TY2810 <yolfry@gmail.com>
2025-01-11 02:04:57 -0400

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export', // Habilita la exportación estática
-  distDir: 'out',  // Esta es la carpeta de salida para GitHub Pages
+  output: 'export',  // Habilita la exportación estática
+  distDir: 'out',    // La carpeta de salida de los archivos estáticos
+  reactStrictMode: true,  // Opcional, pero es recomendable para detectar problemas
 };
 
 export default nextConfig;
