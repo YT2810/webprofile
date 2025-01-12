eb9bea6a1d6367dbc3da7ec9ad783e17dc4852a5
TY2810 <yolfry@gmail.com>
2025-01-12 13:40:47 -0400

    Update images to Next.js Image component

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
       {/* Foto y nombre */}
       <div className="profile">
         <Image
-          src="/img/profile.jpg"
+          src="webprofile/img/profile.jpg"
           alt="Mi Foto de Perfil"
           width={120}
           height={120}
