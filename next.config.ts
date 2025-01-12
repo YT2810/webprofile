61d858a0c1272a17134651f4de0801d4a89135c1
TY2810 <yolfry@gmail.com>
2025-01-12 13:31:18 -0400

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',  // Habilita la exportación estática
   distDir: 'out',    // La carpeta de salida de los archivos estáticos
+  basePath: '/webprofile', // El prefijo del repositorio en GitHub Pages
   reactStrictMode: true,  // Opcional, pero es recomendable para detectar problemas
 };
 
--- out/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iMO0eOIID64t03o9jEogWSm/OdA3Tuff+tnSvBHAioA=\"\n}"
