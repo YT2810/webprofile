e2cac4bcc73a1a5dfc75b97d6511760491353313
TY2810 <yolfry@gmail.com>
2025-01-13 00:46:03 -0400

    Agregar carpeta out con archivos estáticos

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,16 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  // Condicional para el basePath
+  basePath: isProd ? '/webprofile' : '', // Si estamos en producción, usa '/webprofile'; si no, deja vacío
   output: 'export',  // Habilita la exportación estática
   distDir: 'out',     // La carpeta de salida de los archivos estáticos
   reactStrictMode: true,
-   images: {
+   
+  images: {
     unoptimized: true,  // Desactiva la optimización de imágenes para exportación estática
   },
-  // Condicional para el basePath
-  basePath: isProd ? '/webprofile' : '', // Si estamos en producción, usa '/webprofile'; si no, deja vacío
+  
 };
 
 export default nextConfig;
